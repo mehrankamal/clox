f3571d400f85306320352b2637e21918be52e796
Mehran Kamal <29648490+mehrankamal@users.noreply.github.com>
2024-10-19 15:30:52 +0500

    Implement basic disassembler for printing the bytecode data.

---
+++ include/debug.h
@@ -0,0 +1,9 @@
+#ifndef CLOX_DEBUG_H
+#define CLOX_DEBUG_H
+
+#include "chunk.h"
+
+void disassemble_chunk(Chunk *chunk, const char *name);
+int disassemble_instruction(Chunk *chunk, int offset);
+
+#endif

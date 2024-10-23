cb61a3a63a28eaf8aaf58812ad6cc596e732e777
Mehran Kamal <29648490+mehrankamal@users.noreply.github.com>
2024-10-23 19:42:34 +0500

    Implement tagged union types to support dynamic typing.

--- src/vm.c
+++ src/vm.c
@@ -1,3 +1,4 @@
+#include <stdarg.h>
 #include <stdio.h>
 
 #include "common.h"
@@ -8,17 +9,26 @@
 
 VM vm;
 
+static Value peek(int distance);
+static void runtime_error(const char *format, ...);
+
 static InterpretResult run()
 {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
-#define BINARY_OP(op)     \
-    do                    \
-    {                     \
-        double b = pop(); \
-        double a = pop(); \
-        push(a op b);     \
+#define BINARY_OP(value_type, op)                       \
+    do                                                  \
+    {                                                   \
+        if (!IS_NUMBER(peek(0)) || !IS_NUMBER(peek(1))) \
+        {                                               \
+            runtime_error("Operands must be numbers."); \
+            return INTERPRET_RUNTIME_ERROR;             \
+        }                                               \
+        double b = AS_NUMBER(pop());                    \
+        double a = AS_NUMBER(pop());                    \
+        push(value_type(a op b));                       \
     } while (false)
+
     for (;;)
     {
 #ifdef DEBUG_TRACE_EXECUTION
@@ -43,20 +53,25 @@ static InterpretResult run()
             break;
         }
         case OP_ADD:
-            BINARY_OP(+);
+            BINARY_OP(NUMBER_VAL, +);
             break;
         case OP_SUB:
-            BINARY_OP(-);
+            BINARY_OP(NUMBER_VAL, -);
             break;
         case OP_MUL:
-            BINARY_OP(*);
+            BINARY_OP(NUMBER_VAL, *);
             break;
         case OP_DIV:
-            BINARY_OP(/);
+            BINARY_OP(NUMBER_VAL, /);
             break;
         case OP_NEGATE:
         {
-            push(-pop());
+            if (!IS_NUMBER(peek(0)))
+            {
+                runtime_error("Operand must be a number.");
+                return INTERPRET_RUNTIME_ERROR;
+            }
+            push(NUMBER_VAL(-AS_NUMBER(pop())));
             break;
         }
         case OP_RETURN:
@@ -76,6 +91,21 @@ static void reset_stack()
     vm.stack_top = vm.stack;
 }
 
+static void runtime_error(const char *format, ...)
+{
+    va_list args;
+    va_start(args, format);
+    vfprintf(stderr, format, args);
+    va_end(args);
+
+    fputs("\n", stderr);
+
+    size_t instruction = vm.ip - vm.chunk->code - 1;
+    int line = vm.chunk->lines[instruction];
+    fprintf(stderr, "[line %d] in script\n", line);
+    reset_stack();
+}
+
 void push(Value value)
 {
     *vm.stack_top = value;
@@ -86,6 +116,11 @@ Value pop()
 {
     vm.stack_top--;
     return *vm.stack_top;
+}
+
+static Value peek(int distance)
+{
+    return vm.stack_top[-1 - distance];
 }
 
 void init_vm()

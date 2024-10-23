c24b3221bcabb04a6d187228f9f13019d942af6f
Mehran Kamal <29648490+mehrankamal@users.noreply.github.com>
2024-10-23 22:21:07 +0500

    Implement the comparision operators for Lox.

--- include/value.h
+++ include/value.h
@@ -38,6 +38,7 @@ typedef struct
     Value *values;
 } ValueArray;
 
+bool values_equal(Value a, Value b);
 void init_value_array(ValueArray *array);
 void write_value_array(ValueArray *array, Value value);
 void free_value_array(ValueArray *array);
--- src/value.c
+++ src/value.c
@@ -44,3 +44,19 @@ void print_value(Value value)
         break;
     }
 }
+
+bool values_equal(Value a, Value b)
+{
+    if (a.type != b.type)
+        return false;
+
+    switch (a.type)
+    {
+    case VAL_BOOL:
+        return AS_BOOL(a) == AS_BOOL(b);
+    case VAL_NUMBER:
+        return AS_NUMBER(a) == AS_NUMBER(b);
+    case VAL_NIL:
+        return true;
+    }
+}

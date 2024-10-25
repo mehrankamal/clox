7aa41cc2229a891289cdb24cd53bc3cadb503c70
Mehran Kamal <29648490+mehrankamal@users.noreply.github.com>
2024-10-25 11:50:09 +0500

    Implement hash tables for Lox and add string interning to dedup the string allocation.

--- src/object.c
+++ src/object.c
@@ -1,8 +1,9 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
 #include "memory.h"
 #include "object.h"
+#include "table.h"
 #include "value.h"
 #include "vm.h"
 
@@ -20,25 +21,58 @@ static Obj *allocate_object(size_t size, ObjType type)
     return object;
 }
 
-static ObjString *allocate_string(char *chars, int length)
+static ObjString *allocate_string(char *chars, int length, uint32_t hash)
 {
     ObjString *string = ALLOCATE_OBJ(ObjString, OBJ_STRING);
-    string -> length = length;
-    string -> chars = chars;
+    string->length = length;
+    string->chars = chars;
+    string->hash = hash;
+    table_set(&vm.strings, string, NIL_VAL);
+
     return string;
+}
+
+static uint32_t hash_string(const char *key, int length)
+{
+    uint32_t hash = 2166136261u;
+    for (int i = 0; i < length; i++)
+    {
+        hash ^= (uint8_t)key[i];
+        hash *= 16777619;
+    }
+
+    return hash;
 }
 
 ObjString *take_string(char *chars, int length)
 {
-    return allocate_string(chars, length);
+    uint32_t hash = hash_string(chars, length);
+
+    ObjString* interned = table_find_string(&vm.strings, chars, length, hash);
+
+    if(interned != NULL)
+    {
+        FREE_ARRAY(char, chars, length + 1);
+        return interned;
+    }
+
+    return allocate_string(chars, length, hash);
 }
 
 ObjString *copy_string(const char *chars, int length)
 {
+    uint32_t hash = hash_string(chars, length);
+
+    ObjString *interned = table_find_string(&vm.strings, chars, length, hash);
+
+    if (interned != NULL)
+        interned;
+
     char *heap_chars = ALLOCATE(char, length + 1);
     memcpy(heap_chars, chars, length);
     heap_chars[length] = '\0';
-    return allocate_string(heap_chars, length);
+
+    return allocate_string(heap_chars, length, hash);
 }
 
 void print_object(Value value)
@@ -48,7 +82,7 @@ void print_object(Value value)
     case OBJ_STRING:
         printf("%s", AS_CSTRING(value));
         break;
-    
+
     default:
         break;
     }
